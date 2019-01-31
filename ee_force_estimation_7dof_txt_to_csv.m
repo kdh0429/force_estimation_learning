daa6bc0f00c7ef0237a59085fd6ba708e7c79eb1
Donghyeon Kim <kdh0429@snu.ac.kr>
2019-01-31 14:11:25 +0900

    Automate data collection and collect 140 training data set

--- ee_force_estimation_7dof_txt_to_csv.m
+++ ee_force_estimation_7dof_txt_to_csv.m
@@ -1,8 +1,8 @@
 RawData = load('training_data.txt');
 
-Training = RawData(randperm(fix(size(RawData,1)*1.0)),:);
+Training = RawData(randperm(fix(size(RawData,1)*0.7)),:);
 Validation = RawData(randperm(fix(size(RawData,1)*0.2)),:);
-Testing = RawData(randperm(fix(size(RawData,1)*0.2)),:);
+Testing = RawData(randperm(fix(size(RawData,1)*0.1)),:);
 
 csvwrite('raw_data_.csv', RawData);
 csvwrite('training_data_.csv', Training);
