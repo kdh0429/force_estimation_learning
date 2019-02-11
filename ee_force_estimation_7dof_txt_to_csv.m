d61ff50a120fd091f6d7137950f46d065bc9ee32
Donghyeon Kim <kdh0429@snu.ac.kr>
2019-02-11 15:12:28 +0900

    Create .csv files for training, validation, testing

--- ee_force_estimation_7dof_txt_to_csv.m
+++ ee_force_estimation_7dof_txt_to_csv.m
@@ -1,10 +1,10 @@
-RawData = load('training_data.txt');
+RawData = load('training_data_episode_290.txt');
 
-Training = RawData(randperm(fix(size(RawData,1)*0.7)),:);
-Validation = RawData(randperm(fix(size(RawData,1)*0.2)),:);
-Testing = RawData(randperm(fix(size(RawData,1)*0.1)),:);
+Training = RawData(randperm(fix(size(RawData,1)*0.0)),:);
+Validation = RawData(randperm(fix(size(RawData,1)*0.0)),:);
+Testing = RawData(randperm(fix(size(RawData,1)*1.0)),:);
 
 csvwrite('raw_data_.csv', RawData);
-csvwrite('training_data_.csv', Training);
-csvwrite('validation_data_.csv', Validation);
-csvwrite('testing_data_.csv', Testing);
+%csvwrite('training_data_.csv', Training);
+%csvwrite('validation_data_.csv', Validation);
+%csvwrite('testing_data_.csv', Testing);
