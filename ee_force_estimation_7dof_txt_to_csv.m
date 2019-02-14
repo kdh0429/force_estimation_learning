09adc77bc41a9e28a17ee4cdd860add94e92c526
Donghyeon Kim <kdh0429@snu.ac.kr>
2019-02-14 17:40:07 +0900

    Create 300 collision test data

--- ee_force_estimation_7dof_txt_to_csv.m
+++ ee_force_estimation_7dof_txt_to_csv.m
@@ -5,6 +5,6 @@ Validation = RawData(randperm(fix(size(RawData,1)*0.0)),:);
 Testing = RawData(randperm(fix(size(RawData,1)*1.0)),:);
 
 csvwrite('raw_data_.csv', RawData);
-csvwrite('training_data_.csv', Training);
+%csvwrite('training_data_.csv', Training);
 %csvwrite('validation_data_.csv', Validation);
-%csvwrite('testing_data_.csv', Testing);
+csvwrite('testing_data_.csv', Testing);
